86fed83805292b7d82a094a935405e7d2aafefb1
Benoit Borrel <benoit@borrel.com>
2019-09-05 11:19:20 -0400

    example-statistics.php: fix how getNbCitationsPerYear() is called as it returns an array and not a string as it was expected in false example code

--- doc/example-statistics.php
+++ doc/example-statistics.php
@@ -19,13 +19,23 @@ $parser = new StatisticsParser($crawler->getCrawler());
 /** @var Statistics $statistics */
 $statistics = new Statistics($parser->parse());
 
+$nbCitationsPerYear = $statistics->getNbCitationsPerYear();
+$sinceYear = $statistics->getSinceYear();
+
+$nbCitationsSinceYear = 0;
+foreach ($nbCitationsPerYear as $year => $nbCitations) {
+    if ($year >= $sinceYear) {
+        $nbCitationsSinceYear += $nbCitations;
+    }
+}
+
 // display statistics
-echo sprintf("           All\t%4d\n", $statistics->getSinceYear());
-echo sprintf("Citations: %4d\t%4d\n", $statistics->getNbCitations(), $statistics->getNbCitationsPerYear());
+echo sprintf("           All\t%4d\n", $sinceYear);
+echo sprintf("Citations: %4d\t%4d\n", $statistics->getNbCitations(), $nbCitationsSinceYear);
 echo sprintf("h-index  : %4d\t%4d\n", $statistics->getHIndex(), $statistics->getHIndexSince());
 echo sprintf("i10-index: %4d\t%4d\n", $statistics->getI10Index(), $statistics->getI10IndexSince());
 echo "\n";
-echo implode("\t", array_keys($statistics->getNbCitationsPerYear()));
+echo implode("\t", array_keys($nbCitationsPerYear));
 echo "\n";
-echo implode("\t", array_values($statistics->getNbCitationsPerYear()));
+echo implode("\t", array_values($nbCitationsPerYear));
 echo "\n";
